90ac055fd6a6ea6a1dc925cc0968fa2d65258305
Riya Singh <riyasingheng01@gmail.com>
2026-08-08 23:37:29 +0530

    JavaScript File

---
+++ JavaScript Part-1/variables.js
@@ -0,0 +1,16 @@
+// Variables in JavaScript
+a = 10;
+console.log(a);
+
+b = 23;
+console.log(b);
+
+sum = a + b;
+console.log(a + b);
+
+// Variable Names
+price = 10;
+price__ = 10;
+price$ = 10;
+$ = 90;
+console.log($);
